bc17d2b83c4b3b10c056aa45f0e87855f523b0d2
shaheryar <muhammadshaheryar50@gmail.com>
2024-09-29 17:24:41 +0500

    navbar done

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from "../assets/logo.png"
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+    const navigation=[
+        {label:"TV Shows",href:"tv"},
+        {label:"Movies",href:"movies"},
+    ]
+  return (
+    <div className='fixed top-0 w-full h-16 bg-neutral-600 opacity-75'>
+        <div className='container mx-auto px-2 flex items-center h-full'>
+            <div>
+                <img src={logo} alt="logo" width={120}/>
+            </div>
+            <nav className='flex items-center gap-1 ml-5'>
+                {navigation.map((nav,index)=>{
+                    return(
+                        <div>
+                            <NavLink key={nav.label} href={nav.href} className={` px-2 hover:text-neutral-50`}>
+                                {nav.label}
+                            </NavLink>
+                        </div>
+                    )
+                })}
+            </nav>
+        </div> 
+    </div>
+  )
+}
+
+export default Header
